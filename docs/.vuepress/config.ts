a3ec9c472a089569e765ce271f1cbfb3d8b0596f
MoomHub <2208300539@qq.com>
2022-06-08 16:56:11 +0800

    升级版本

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,10 +1,13 @@
 import { defineUserConfig } from "vuepress";
-import type { DefaultThemeOptions } from "vuepress";
-import type { ClipboardOptions } from 'vuepress-plugin-clipboard'
-import { sidebars,navbar } from "./configs";
+import { defaultTheme } from "@vuepress/theme-default";
+import { path } from "@vuepress/utils";
+import type { ClipboardOptions } from "vuepress-plugin-clipboard";
+import { docsearchPlugin } from "@vuepress/plugin-docsearch";
+import { clipboardPlugin } from "vuepress-plugin-clipboard";
+import { sidebars, navbar } from "./configs";
+const { prismjsPlugin } = require("@vuepress/plugin-prismjs");
 
-
-export default defineUserConfig<DefaultThemeOptions>({
+export default defineUserConfig({
   base: "/",
   head: [
     ["link", { rel: "icon", href: "/images/icon/favicon.ico" }],
@@ -17,8 +20,7 @@ export default defineUserConfig<DefaultThemeOptions>({
   description: "记录学习信息的文档",
 
   // 主题和它的配置
-  theme: "@vuepress/theme-default",
-  themeConfig: {
+  theme: defaultTheme({
     logo: "/images/png/logo.png",
     // 仓库地址
     repo: "https://github.com/moomhub/Notes",
@@ -54,67 +56,75 @@ export default defineUserConfig<DefaultThemeOptions>({
       // only enable git plugin in production mode
       git: true,
     },
+  }),
+  // configure markdown
+  markdown: {
+    importCode: {
+      handleImportPath: (str) =>
+        str.replace(
+          /^@vuepress/,
+          path.resolve(__dirname, "../../packages/@vuepress")
+        ),
+    },
   },
   plugins: [
-    [
-      "@vuepress/docsearch",
-      {
-        appId: "Q6JY0Z6EVF",
-        apiKey: "8644a38510392fdc7e0aabb51371cebd",
-        indexName: "moomhub",
-        locales: {
-          "/": {
-            placeholder: "搜索文档",
-            translations: {
-              button: {
-                buttonText: "搜索文档",
-                buttonAriaLabel: "搜索文档",
+    //文档搜索
+    docsearchPlugin({
+      appId: "Q6JY0Z6EVF",
+      apiKey: "8644a38510392fdc7e0aabb51371cebd",
+      indexName: "moomhub",
+      searchParameters: {
+        facetFilters: ['tags:v2'],
+      },
+      locales: {
+        "/": {
+          placeholder: "搜索文档",
+          translations: {
+            button: {
+              buttonText: "搜索文档",
+              buttonAriaLabel: "搜索文档",
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: "清除查询条件",
+                resetButtonAriaLabel: "清除查询条件",
+                cancelButtonText: "取消",
+                cancelButtonAriaLabel: "取消",
               },
-              modal: {
-                searchBox: {
-                  resetButtonTitle: "清除查询条件",
-                  resetButtonAriaLabel: "清除查询条件",
-                  cancelButtonText: "取消",
-                  cancelButtonAriaLabel: "取消",
-                },
-                startScreen: {
-                  recentSearchesTitle: "搜索历史",
-                  noRecentSearchesText: "没有搜索历史",
-                  saveRecentSearchButtonTitle: "保存至搜索历史",
-                  removeRecentSearchButtonTitle: "从搜索历史中移除",
-                  favoriteSearchesTitle: "收藏",
-                  removeFavoriteSearchButtonTitle: "从收藏中移除",
-                },
-                errorScreen: {
-                  titleText: "无法获取结果",
-                  helpText: "你可能需要检查你的网络连接",
-                },
-                footer: {
-                  selectText: "选择",
-                  navigateText: "切换",
-                  closeText: "关闭",
-                  searchByText: "搜索提供者",
-                },
-                noResultsScreen: {
-                  noResultsText: "无法找到相关结果",
-                  suggestedQueryText: "你可以尝试查询",
-                  openIssueText: "你认为该查询应该有结果？",
-                  openIssueLinkText: "点击反馈",
-                },
+              startScreen: {
+                recentSearchesTitle: "搜索历史",
+                noRecentSearchesText: "没有搜索历史",
+                saveRecentSearchButtonTitle: "保存至搜索历史",
+                removeRecentSearchButtonTitle: "从搜索历史中移除",
+                favoriteSearchesTitle: "收藏",
+                removeFavoriteSearchButtonTitle: "从收藏中移除",
+              },
+              errorScreen: {
+                titleText: "无法获取结果",
+                helpText: "你可能需要检查你的网络连接",
+              },
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+                closeText: "关闭",
+                searchByText: "搜索提供者",
+              },
+              noResultsScreen: {
+                noResultsText: "无法找到相关结果",
+                suggestedQueryText: "你可以尝试查询",
+                reportMissingResultsText: "你认为该查询应该有结果？",
+                reportMissingResultsLinkText: "点击反馈",
               },
             },
           },
         },
-      }
-    ],
-    [
-      'vuepress-plugin-clipboard',
-      {
-        align: "top",
-        staticIcon: true,
-        successText:"复制成功！"
-
-      } as ClipboardOptions,
-    ],
+      },
+    }),
+    prismjsPlugin({}),
+    clipboardPlugin({
+      align: "top",
+      staticIcon: true,
+      successText: "复制成功！",
+    }),
   ],
 });
